7c479a0dc8cc8f7d4737c19bab12df18c57c3959
perrystallings <perry.stallings@synapsefi.com>
2024-01-20 11:39:00 -0600

    Adding Filter Evaluation

--- parrot_integrations/core/common.py
+++ parrot_integrations/core/common.py
@@ -1,54 +1,52 @@
-from importlib import import_module
+def load_integration_module(integration_key: str, operation_key: str = None):
+    from importlib import import_module
 
-
-def load_integration_type(integration_key):
     module = import_module(
-        name=integration_key
-    )
-    return module
-
-
-def load_operation(integration_key, operation_key):
-    module = import_module(
-        name=f'{integration_key}.{operation_key}'
+        name=integration_key if operation_key is None else f'{integration_key}.{operation_key}'
     )
     return module
 
 
 def format_data(record, schema):
-    from jsonpath_ng.ext.parser import parse as ng_parse
     data = dict()
     for k, v in schema.items():
-        val = None
         if isinstance(v, list):
-            val = [format_data(record=record, schema=i) for i in v]
+            value = [format_data(record=record, schema=i) if isinstance(i, dict) else i for i in v]
         elif isinstance(v, dict):
-            if all(i in ['value', 'path', 'default'] for i in v.keys()):
-                if 'value' in v.keys():
-                    val = v['value']
-                elif 'path' in v.keys():
-                    parser = ng_parse(v['path'])
-                    val = [i.value for i in parser.find(record)]
-                    if len(val) == 0:
-                        val = None
-                    elif len(val) == 1:
-                        val = val[0]
-                if val is None and 'default' in v.keys():
-                    val = v['default']
+            if all(i in ['value', 'path', 'default', 'transforms'] for i in v.keys()):
+                value = extract_value(field=v, record=record)
             else:
-                val = v
-        data[k] = val
+                value = {sub_k: extract_value(field=sub_v, record=record) for sub_k, sub_v in v.items()}
+        else:
+            value = v
+        data[k] = value
     return data
 
 
-def evaluate_filter(edge, record):
-    return True
+def apply_transform(value, transform):
+    from parrot_integrations.core import transforms
+    value = getattr(transforms, transform['operator'])(value=value, **transform)
+    return value
 
 
-def validate_edge_filter(edge_filter):
-    return True
-
-
-def validate_workflow_graph(nodes, edges):
-    return True
-
+def extract_value(field, record):
+    from jsonpath_ng.ext import parse as ng_parse
+    value = None
+    if 'path' in field.keys():
+        result = ng_parse(path=field['path']).find(record)
+        if len(result) == 1:
+            value = result.value
+        elif len(result) > 1:
+            if not field.get('use_first_result', True):
+                value = [i.value for i in result]
+            else:
+                value = result[0].value
+    elif 'value' in field.keys():
+        value = field['value']
+    else:
+        raise SyntaxError("Must use 'path' or 'value'")
+    if value is None and field.get('default'):
+        value = field['default']
+    for transform in field.get('transforms', []):
+        value = apply_transform(value=value, transform=transform)
+    return value
